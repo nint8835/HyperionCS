3f4c5d7b69be0a0b800ad3522822d39387ffb017
Riley Flynn <riley@rileyflynn.me>
2022-06-07 18:55:13 -0230

    Get project working again
    I have no clue how this was left in such a broken state

--- hyperioncs/config.py
+++ hyperioncs/config.py
@@ -10,7 +10,9 @@ class Config(BaseSettings):
     jwt_algorithm: str = "HS256"
     discord_client_id: str
     discord_client_secret: str
-    sqlalchemy_connection_url: str = "postgresql://hyperion:hyperion@localhost/hyperion"
+    sqlalchemy_connection_url: str = (
+        "postgresql+pg8000://hyperion:hyperion@localhost/hyperion"
+    )
 
     # Honeycomb config
     use_honeycomb: bool = False
