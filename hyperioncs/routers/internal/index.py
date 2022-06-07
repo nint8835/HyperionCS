341468c60380fb2e82276960da83971722a2fc47
Riley Flynn <riley@rileyflynn.me>
2022-06-07 21:07:06 -0230

    Omit internal current user from OpenAPI

--- hyperioncs/routers/internal/index.py
+++ hyperioncs/routers/internal/index.py
@@ -6,6 +6,6 @@ from hyperioncs.schemas import DiscordUser
 index_router = APIRouter()
 
 
-@index_router.get("/users/me", response_model=DiscordUser)
+@index_router.get("/users/me", response_model=DiscordUser, include_in_schema=False)
 def current_user(user: DiscordUser = Depends(get_discord_user)) -> DiscordUser:
     return user
