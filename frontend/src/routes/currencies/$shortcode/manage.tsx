3fde4fa73ad4dd9010bfa389e9de5f07dd5c46ad
Riley Flynn <riley@rileyflynn.me>
2025-12-24 23:34:12 -0330

    Improve integration management UI

--- frontend/src/routes/currencies/$shortcode/manage.tsx
+++ frontend/src/routes/currencies/$shortcode/manage.tsx
@@ -1,8 +1,8 @@
-import { Button, Divider } from '@heroui/react';
+import { Button } from '@heroui/react';
 import { createFileRoute } from '@tanstack/react-router';
 
 import { queryClient } from '@/lib/query';
-import { getCurrencyQuery, useSuspenseGetCurrency } from '@/queries/integrations/v1/integrationsV1Components';
+import { getCurrencyQuery } from '@/queries/integrations/v1/integrationsV1Components';
 import {
   getCurrencyIntegrationsQuery,
   listIntegrationsQuery,
@@ -11,6 +11,7 @@ import {
   useSuspenseGetCurrencyIntegrations,
   useSuspenseListIntegrations,
 } from '@/queries/internal/internalComponents';
+import type { IntegrationSchema } from '@/queries/internal/internalSchemas';
 
 export const Route = createFileRoute('/currencies/$shortcode/manage')({
   component: RouteComponent,
@@ -22,22 +23,41 @@ export const Route = createFileRoute('/currencies/$shortcode/manage')({
     ]),
 });
 
-function ManageIntegrations({ shortcode }: { shortcode: string }) {
-  const { data: integrations } = useSuspenseListIntegrations({});
-  const { data: currencyIntegrations } = useSuspenseGetCurrencyIntegrations({ pathParams: { shortcode } });
-
+function IntegrationDisplay({
+  integration,
+  isConnected = false,
+  currencyShortcode,
+}: {
+  integration: IntegrationSchema;
+  isConnected?: boolean;
+  currencyShortcode: string;
+}) {
   const { mutateAsync: connectIntegration } = useConnectIntegration();
   const { mutateAsync: disconnectIntegration } = useDisconnectIntegration();
 
-  async function doConnect(integrationId: string) {
-    await connectIntegration({ pathParams: { integrationId }, body: { currency_shortcode: shortcode } });
-    queryClient.invalidateQueries(getCurrencyIntegrationsQuery({ pathParams: { shortcode } }));
+  const stateToggler = isConnected ? disconnectIntegration : connectIntegration;
+
+  async function toggleState(integrationId: string) {
+    await stateToggler({ pathParams: { integrationId }, body: { currency_shortcode: currencyShortcode } });
+    await queryClient.invalidateQueries(getCurrencyIntegrationsQuery({ pathParams: { shortcode: currencyShortcode } }));
   }
 
-  async function doDisconnect(integrationId: string) {
-    await disconnectIntegration({ pathParams: { integrationId }, body: { currency_shortcode: shortcode } });
-    queryClient.invalidateQueries(getCurrencyIntegrationsQuery({ pathParams: { shortcode } }));
-  }
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div>
+        <h4 className="font-semibold">{integration.name}</h4>
+        <p className="text-default-500 text-sm">{integration.description}</p>
+      </div>
+      <Button onPress={() => toggleState(integration.id)} color={isConnected ? 'danger' : 'primary'}>
+        {isConnected ? 'Disconnect' : 'Connect'}
+      </Button>
+    </div>
+  );
+}
+
+function ManageIntegrations({ shortcode }: { shortcode: string }) {
+  const { data: integrations } = useSuspenseListIntegrations({});
+  const { data: currencyIntegrations } = useSuspenseGetCurrencyIntegrations({ pathParams: { shortcode } });
 
   const availablePrivateIntegrations = integrations.filter(
     (integration) => integration.private && !currencyIntegrations.some((ci) => ci.id === integration.id),
@@ -53,22 +73,20 @@ function ManageIntegrations({ shortcode }: { shortcode: string }) {
       <div>
         <h3 className="text-xl font-semibold">Connected Integrations</h3>
         {currencyIntegrations.length === 0 ? (
-          <p>No integrations connected.</p>
+          <p className="text-default-400 w-full text-center italic">No integrations connected.</p>
         ) : (
-          <ul>
+          <div className="space-y-1">
             {currencyIntegrations.map((integration) => (
-              <div key={integration.id}>
-                {integration.name}{' '}
-                <Button color="danger" onPress={() => doDisconnect(integration.id)}>
-                  Disconnect
-                </Button>
-              </div>
+              <IntegrationDisplay
+                key={integration.id}
+                integration={integration}
+                isConnected
+                currencyShortcode={shortcode}
+              />
             ))}
-          </ul>
+          </div>
         )}
       </div>
-
-      <Divider />
 
       <div>
         <h3 className="text-xl font-semibold">Available Integrations</h3>
@@ -76,30 +94,26 @@ function ManageIntegrations({ shortcode }: { shortcode: string }) {
         <div>
           <h4 className="text-lg font-semibold">Private Integrations</h4>
           {availablePrivateIntegrations.length === 0 ? (
-            <p>No private integrations available.</p>
+            <p className="text-default-400 w-full text-center italic">No private integrations available.</p>
           ) : (
-            <ul>
+            <div className="space-y-1">
               {availablePrivateIntegrations.map((integration) => (
-                <div key={integration.id}>
-                  {integration.name} <Button onPress={() => doConnect(integration.id)}>Connect</Button>
-                </div>
+                <IntegrationDisplay key={integration.id} integration={integration} currencyShortcode={shortcode} />
               ))}
-            </ul>
+            </div>
           )}
         </div>
 
         <div>
           <h4 className="text-lg font-semibold">Public Integrations</h4>
           {availablePublicIntegrations.length === 0 ? (
-            <p>No public integrations available.</p>
+            <p className="text-default-400 w-full text-center italic">No public integrations available.</p>
           ) : (
-            <ul>
+            <div className="space-y-1">
               {availablePublicIntegrations.map((integration) => (
-                <div key={integration.id}>
-                  {integration.name} <Button onPress={() => doConnect(integration.id)}>Connect</Button>
-                </div>
+                <IntegrationDisplay key={integration.id} integration={integration} currencyShortcode={shortcode} />
               ))}
-            </ul>
+            </div>
           )}
         </div>
       </div>
@@ -109,7 +123,6 @@ function ManageIntegrations({ shortcode }: { shortcode: string }) {
 
 function RouteComponent() {
   const { shortcode } = Route.useParams();
-  const { data: currency } = useSuspenseGetCurrency({ pathParams: { shortcode } });
 
   return (
     <div>
